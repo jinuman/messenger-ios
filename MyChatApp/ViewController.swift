e4fe6baf37b8af576ca5fc1cf4c921e5dbd5b4b0
jinuman <jin35kim@gmail.com>
2019-02-09 14:27:18 +0900

    Initialize without Main.storyboard

---
+++ MyChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyChatApp
+//
+//  Created by Jinwoo Kim on 09/02/2019.
+//  Copyright © 2019 jinuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
